9b411096f3b98dce6d7054073b51c1447eb0967d
IN1DM3B_18 <IN1DM3B_18@HZ317813.H015112.NET>
2019-01-10 13:56:46 +0100

    1ercommit. inicio de mi proyecto (aprendiendo github).

---
+++ src/pruebagithub/aprendiendocosasdegit.java
@@ -0,0 +1,10 @@
+package pruebagithub;
+
+public class aprendiendocosasdegit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hola gente");
+	}
+
+}
